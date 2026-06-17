c880e59ab11b25adca13c271392501fe3d7434d1
Avinash Oraon <119616950+AvinashOraon123@users.noreply.github.com>
2026-06-17 21:43:21 +0530

    Add files via upload

---
+++ Day14_SQL/day14.sql
@@ -0,0 +1,7 @@
+UPDATE avinash.users
+-- WHERE price > 10000 AND price <20000
+-- WHERE rating > 80 AND price <15000 AND processor_brand = 'snapdragon'
+-- 	WHERE processor_brand = 'snapdragon' or processor_brand = 'exynos' or processor_brand = 'bionic'
+-- WHERE processor_brand IN ('snapdragon' , 'exynos' , 'bionic')
+SET email = 'avi@gmail.com' , password = '234'
+WHERE name = 'avinash'
